f9be3f18c776c566040fdad9b469320b45b7b475
kingquote <45900034+kingquote@users.noreply.github.com>
2021-03-09 14:09:46 +0100

    ..

---
+++ UZH-files/Bio206/L12_Segregation.py
@@ -0,0 +1,44 @@
+import pycxsimulator
+from pylab import *
+
+n = 1000 # number of agents
+r = 0.1 # neighbourhood radius
+thresh = 0.5 # threshold for moving
+
+class agent:
+    pass
+
+def initialise():
+    global agents
+    agents = []
+    for i in range(n):
+        ag = agent()
+        ag.type = randint(2)
+        ag.x = random()
+        ag.y = random()
+        agents.append(ag)
+    
+def observe():
+    global agents
+    cla()
+    red = [ag for ag in agents if ag.type == 0]
+    blue = [ag for ag in agents if ag.type == 1]
+    plot([ag.x for ag in red], [ag.y for ag in red], 'ro')
+    plot([ag.x for ag in blue], [ag.y for ag in blue], 'bo')
+    axis('image')
+    axis([0, 1, 0, 1])
+
+def update():
+    global agents
+    ag = choice(agents)
+    neighbours = [nb for nb in agents
+                 if (ag.x - nb.x)**2 + (ag.y - nb.y)**2 < r**2 and nb != ag]
+    if len(neighbours) > 0:
+        q = len([nb for nb in neighbours if nb.type == ag.type]) \
+            / float(len(neighbours))
+        if q < thresh:
+            ag.x, ag.y = random(), random()
+
+pycxsimulator.GUI().start(func=[initialise, observe, update])
+
+
